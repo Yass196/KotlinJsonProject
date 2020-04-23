504662b90b16ca9c5bb98028eec6f3f92a981e0f
yassine <yassineyounsii@gmail.com>
2020-04-23 22:08:49 +0200

    api url fix

--- app/src/main/java/com/example/kotlinjsonproject/Common/Common.kt
+++ app/src/main/java/com/example/kotlinjsonproject/Common/Common.kt
@@ -4,7 +4,7 @@ import com.example.kotlinjsonproject.Interface.RetrofitService
 import com.example.kotlinjsonproject.Retrofit.RetrofitClient
 
 object Common {
-    private val URL = "https://www.simplifiecoding;net/demo/"
+    private val URL = "https://www.simplifiedcoding.net/demos/"
     val RETROSERVICE : RetrofitService
     get() = RetrofitClient.getClient(URL).create(RETROSERVICE::class.java)
 }
--- app/src/main/java/com/example/kotlinjsonproject/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/kotlinjsonproject/Retrofit/RetrofitClient.kt
@@ -1,5 +1,4 @@
 package com.example.kotlinjsonproject.Retrofit
-
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
